0309b7aa72ab12eb8ab9808d8fe1c78f03754747
solattila <solattila@gmail.com>
2023-02-17 15:05:19 +0100

    "add logger"

---
+++ decorators.py
@@ -0,0 +1,15 @@
+def logger(function):
+    def wrapper(*args, **kwargs):
+        print(f"Function {function.__name__}() was called")
+        result = function(*args, **kwargs)
+        print(f"Function {function.__name__}() returned {result}")
+        return result
+    return wrapper
+
+@logger
+def add_two_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    add_two_numbers(3, 5)
